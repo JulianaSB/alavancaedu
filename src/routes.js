b6c6b0ea343ddeecee463b529631c79ff684d4a7
Giovanna Gomes <39038412+GiovannaSGomes@users.noreply.github.com>
2020-07-28 19:41:20 -0300

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
+//import Login from './pages/Login';
+//import Test from './pages/Test';
+import Question from './pages/Prova'
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Login} />
-                <Route path="/profile" exact component={Profile} />
+                
+                
+                <Route path="/" exact component={Question} />
             </Switch>
         </BrowserRouter>
     )
 }
+
+//<Route path="/" exact component={Login} /> 
+//<Route path="/" exact component={Test} />
