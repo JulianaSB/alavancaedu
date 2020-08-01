c4bd44a24a2045fc90ad193479cbebba44ba6cf2
juliana_simoesb <j.simoes.brazelino@avanade.com>
2020-07-31 21:31:31 -0300

    Settings name user

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './style.css';
+import api from '../../services/api';
 import iconImg from '../../assests/estrangeiro.png';
 
 
 export default function Profile() {
+    const studentName = localStorage.getItem('name');
     return (
        <div className="chat-container container">
-            <h1 className="mb-4">Oi, Joãozinho</h1>
+            <h1 className="mb-4">Oi, {studentName}</h1>
            <div className="card">
                 <div className="card-body">
                     <div className="row">
